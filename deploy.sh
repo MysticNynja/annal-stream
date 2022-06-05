b8c6fa539e18a7e247aa9056e44ea4ef444f3481
Will York <iamwillyork@outlook.com>
2022-06-04 23:57:01 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,11 @@ npm run build
 
 cd dist
 
+git config --global user.email "iamwillyork@outlook.com"
+git config --global user.name "Will York"
 git init
 git add -A
 git commit -m "New Deployment"
-git config --global user.email "iamwillyork@outlook.com"
-git config --global user.name "Will York"
 git push -f git@github.com:MysticNynja/annal-stream.git master:gh-pages
 
 cd -
