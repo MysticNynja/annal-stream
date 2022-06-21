7499aa85bd889167751c3f0d1b97a84c3445dae1
wyork <wyork@synchrono.com>
2022-06-21 13:39:36 -0500

    adding vuetify v3

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <template>
-<!-- This serves all our pages using vue router -->
-<router-view v-slot="{ Component }">
-  <transition name="fade" mode="out-in">
-    <component :is="Component" :key="$route.path"/> 
-  </transition>
-</router-view>
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 
 export default {
   name: 'App',
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .4s;
-}
-.fade-enter, .fade-leave-to {
-  opacity: 0;
-}
-</style>
